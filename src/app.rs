eab651b972930a1aa226d84ad0d4693fef0cc02e
timvisee <timvisee@gmail.com>
2017-02-27 15:22:35 +0100

    Add help and version command handling

---
+++ src/app.rs
@@ -0,0 +1,5 @@
+pub const APP_NAME: &'static str = "dotfiler";
+pub const APP_VERSION_NAME: &'static str = "v0.0.1-alpha";
+pub const APP_VERSION_CODE: i32 = 1;
+pub const APP_AUTHOR: &'static str = "Tim Visee <timvisee@gmail.com>";
+pub const APP_DESCRIPTION: &'static str = "A simple but useful tool to manage your dotfiles.";
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+extern crate clap;
+extern crate dotfiler;
+
+use clap::App;
+use dotfiler::app;
+
+/// Application entry point.
 fn main() {
+    // Handle command line arguments for help
+    handle_arguments();
+
     println!("Hello, world!");
+}
+
+/// Handle program arguments passed along with the command line to show things like help pages.
+///
+/// # Examples
+///
+/// Parse application parameters, and show relevant information such as help files in the console:
+/// ```no_run
+/// handle_arguments();
+/// ```
+fn handle_arguments() {
+    // Configure the application object with help information, and show matches
+    App::new(app::APP_NAME)
+        .version(app::APP_VERSION_NAME)
+        .author(app::APP_AUTHOR)
+        .about(app::APP_DESCRIPTION)
+        .get_matches();
 }
